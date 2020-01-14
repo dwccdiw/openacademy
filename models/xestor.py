292fc120d597ee63d8c4e3e9afa87cbfaaef4f00
eu <dwccdiw@gmail.com>
2020-01-14 17:59:17 +0100

    curso3

--- models/xestor.py
+++ models/xestor.py
@@ -6,4 +6,5 @@ class xestor(models.Model):
                             # por tanto non se crea unha nova clase e os atributos engadense na clase pai
                             # No xml das vistas temos que extender as vistas da clase pai
     avatar = fields.Char (required=True, string="Avatar")
+    apelidos = fields.Char( required=True, string="Apelidos")
 
--- models/xestor2.py
+++ models/xestor2.py
@@ -5,6 +5,6 @@ class xestor2(models.Model):
     _name = 'openacademy.xestor2'# proporcionamos  _name entón facemos prototipo de herdanza
                             # por tanto  se crea unha nova táboa
                             # No xml das vistas temos que extender as vistas da clase pai
-    _inherit = 'res.partner'  # como  non lle proporcioamos  _name facemos herdanza por extensión (herdanza de clase)
+    _inherit = 'res.partner'
     avatar2 = fields.Char (required=True, string="Avatar2")
 
